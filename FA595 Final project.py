96ea56a596ccda4d33906e4db4e479e7657d3dc7
zemin13214081003 <90585667+zemin13214081003@users.noreply.github.com>
2021-11-25 01:43:10 -0500

    Add files via upload

--- FA595 Final project.py
+++ FA595 Final project.py
@@ -1,2 +1,11 @@
-Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+from Management Opportunities import SentimentIntensityAnalyzer
+analyzer=SentimentIntensityAnalyzer()
+def sentence="we cannot predict the duration of the COVID-19 pandemic"
+score=analyzer.polarity_soces(sentence)
+print"{:-<60]} {}".format(sentence,str(score))
+
+sentment_analyzer_scores("On the other hand")
+print(sentment_analyzer_scores("On the other hand"))
+
+sentment_analyzer_scores("we will have to accurately project demand and infrastructure requirements globally")
+print(sentment_analyzer_scores("we will have to accurately project demand and infrastructure requirements globally"))
